4aa613cd875798d697bae5d3387aae3105e43c72
Madeleine Bruce <maxskyebruce@gmail.com>
2023-01-31 17:37:46 -0800

    A2-1

--- main.py
+++ main.py
@@ -3,13 +3,15 @@ def main():
    # Comlete your code here
    ##################################################
     
+    # original_price = int(original_price)
     original_price = int(200)
+    
     rate = int(0.20)
-    discount_amount = int(40)
-    final_sales_price = original_price - discount_amount
+    discount_amount = original_price * rate
     print(original_price)
     print(discount_amount)
-    print(final_sales_price)
+    print(original_price - discount_amount)
+    
     pass
 
 
